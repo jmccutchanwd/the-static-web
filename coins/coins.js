ab2440dad86f6ee5ef6c8fda14ba8b3399dde2bf
John McCutchan <john.mccutchan.wd@gmail.com>
2016-11-28 23:13:34 -0600

    complete

--- coins/coins.js
+++ coins/coins.js
@@ -5,14 +5,17 @@ Counting Coins
 
 Setup
 
-These commands are a helpful quick start. You may choose to ignore them completely and create your own directory structure. If you choose to use this recommendation, just copy the commands below. It doesn't matter what directory you are currently in.
+These commands are a helpful quick start. You may choose to ignore them completely 
+and create your own directory structure. If you choose to use this recommendation, 
+just copy the commands below. It doesn't matter what directory you are currently in.
 
 mkdir -p ~/workspace/exercises/the-static-web/coins && cd $_
 touch index.html
 touch coins.js
 Instructions
 
-Write a program that will convert a dollar amount to the number of coins that make up the amount. Use the common American coins of quarters, dimes, nickels, and pennies.
+Write a program that will convert a dollar amount to the number of coins that make 
+up the amount. Use the common American coins of quarters, dimes, nickels, and pennies.
 
 /*
   Input: 0.67
@@ -25,24 +28,113 @@ Write a program that will convert a dollar amount to the number of coins that ma
   }
 
 */
+var money = 0;
+var coin="";
+
 function myFunction() {
-    var person = prompt("Please enter your name", "Harry Potter");
-    
-    if (person != null) {
-        document.getElementById("demo").innerHTML= "Hello " + person + "! How are you today?";
-    }
+    var getAmount = prompt("Please enter a dollar amount");
+    console.log(getAmount);
+    money = getAmount * 100;
+    console.log(money);
+    coin = "<li>"+" Amount Entered: "+getAmount+"</li>"+"<br>";
+    var result = document.getElementById("entered");
+	result.innerHTML = coin;
+    coinCounter();
 }
 
 function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
+                         
+	var coinPurse = { // Initialize a JavaScript object to hold the coins
+		quarters:0,
+		dimes:0,
+		nickels:0,
+		pennies:0
+	};
+  	console.log(coinPurse);	
+  	for (i = 0; i<= money; i++) {
+		if (money >= 25) {
+	  		coinPurse.quarters += 1;		
+	  		money -= 25;
+	  		console.log(coinPurse.quarters);
+	    	console.log("Money quarters " + money);
+	    }
+	}    
+	for (i = 0; i<= money; i++) {
+		if(money >= 10) {
+	    	coinPurse.dimes += 1;
+	    	money -= 10;
+	    	console.log(coinPurse.dimes);
+	    	console.log("Money dimes " + money);
+	    }
+	}
+	for (i = 0; i<= money; i++) {   
+	    if(money >= 5) {
+	    	coinPurse.nickels += 1;
+	    	money -= 5;
+	    	console.log(coinPurse.nickels);
+	    	console.log("Money nickels " + money);
+	    }
+	}
+	for (i = 0; i<= money + 1; ++i) {
+		if (money % 1 === true) {
+			money = Math.floor(money);
+	    	console.log("After floor: " + money);
+	    } else if (money >= 0) {
+	    	coinPurse.pennies += 1;
+	    	money -= 1;
+	    	console.log(coinPurse.pennies);
+	    	console.log("Money pennies " + money);
+	    }
+	}
+	console.log("The remainder is " + money);
+	console.log(coinPurse);
+	
+	coin = "<li>"+coinPurse.quarters+" Quarters"+"</li>"+"<br>"+
+		   "<li>"+coinPurse.dimes+" Dimes"+"</li>"+"<br>"+
+		   "<li>"+coinPurse.nickels+" Nickels"+"</li>"+"<br>"+
+		   "<li>"+coinPurse.pennies+" Pennies"+"</li>"+"<br>";
 
-  coinPurse.quarters = 0;
-
-  return coinPurse;
+	var result = document.getElementById("coins");
+	result.innerHTML = coin;
 }
 
-var coins = coinCounter();
-console.log();
+
+
+// currentBand += "<li>"+bands[i]+"</li>";// Add the band names into the correct <div>
+//     currentVeggie += "<li>"+vegetables[i]+"</li>";// Add the veggie names into the correct <div>
+//     console.log(currentBand + currentVeggie);//THis is to see whats happening behind the scenes.
+// }
+
+// var bandElement = document.getElementById("boy-bands");//creates a Var to equal the OL that is 
+// 													   //tagged in the html
+// bandElement.innerHTML = currentBand;//creates the LI list as looped from the For loop
+
+
+
+
+// var coins = coinCounter();
+// console.log();
+
+
+
+//divide amount by quarters subtract from amount(loop counter is returned to coinpurse)
+//divide amount by dimes subtract from amount(loop counter is returned to coinpurse)
+//divide amount by nickels subtract from amount(loop counter is returned to coinpurse)
+//divide amount by pennies(loop counter is returned to coinpurse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
