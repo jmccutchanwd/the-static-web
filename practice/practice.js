d5e8cffdcd9acf52e5803b4513dd84d86686fd5c
John McCutchan <john.mccutchan.wd@gmail.com>
2016-11-29 13:12:45 -0600

    initial

---
+++ practice/practice.js
@@ -0,0 +1,9 @@
+//push, pop
+//unshift, shift
+
+//indexOf good to use to see if something exists
+
+//splice, slice *splice-start, delete count, elements 
+// to be added *slice-start, number after last one, this is used to copy a slice
+
+//join *join pass a seperator in the argument
