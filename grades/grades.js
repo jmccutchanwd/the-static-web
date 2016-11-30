9c289e0ed391b8ec183fc3b639061d2269d36a5b
John McCutchan <john.mccutchan.wd@gmail.com>
2016-11-30 15:21:08 -0600

    added JS to sort grades, added styling to html

--- grades/grades.js
+++ grades/grades.js
@@ -19,3 +19,47 @@ How many of each grade?
 What is the lowest grade?
 What is the highest grade?
 */
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var button = document.getElementById('now');
+button.addEventListener('click', sortGrades);
+var gradeA = 0;
+var gradeB = 0;
+var gradeC = 0;
+var gradeD = 0;
+var gradeF = 0;
+
+function sortGrades(){
+	console.log("button clicked");
+	for(var i = 0; i < scores.length; i++) {
+		// console.log("number " + i);
+		if(scores[i] >= 91){
+			gradeA += 1;
+		}else if(scores[i] >= 81 && scores[i] <= 90){
+			gradeB += 1;
+		}else if(scores[i] >= 71 && scores[i] <= 80){
+			gradeC += 1;
+		}else if(scores[i] >= 61 && scores[i] <= 70){
+			gradeD += 1;
+		}else{
+			gradeF += 1;
+		}
+		
+	}
+	console.log("A:" + gradeA + ", " + "B:" + gradeB + ", " + "C:" + gradeC + ", "
+		+ "D:" + gradeD + ", " + "F:" + gradeF);
+}
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
